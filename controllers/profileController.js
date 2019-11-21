c9f73faa8fb93655c70e258f1e7e0a30cce9deab
Jdavies91 <Jdavies86@my.bcit.ca>
2019-11-20 18:21:27 -0800

    Adding User Profile

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,21 +1,87 @@
-const db = require('../model/database');
+const userdb = require('../models').User;
 
-const profileCreator1 = (req, res, next) => {
-    db.creatingProfileStepOne(req,res);
-
-  };
-
-const profileCreator2 = (req, res, next) => {
-    db.creatingProfileStepTwo(req,res);
-
-  };
+// const profileCreator1 = (res, next) => {
+// Had to block out comment user to make this work
+   function  usercreate1(req, res) {
+    return userdb.create({
+            firstName: "Jonathan ",
+            lastName: "Davies",
+            email: "JonDavies4@hotmail.com",
+            password: "Computer",
+        })
+        .then(userdb => res.status(201).send(userdb))
+        .catch(error => res.status(400).send(error));
+    }
+    function  userfindUser(req, res) {
+        return userdb.findOne({
+            email: "JonDavies300@hotmail.com", password: "password"
+          })
+          .then(userdb => {
+            console.log(`Found user: ${userdb.email}`);
+            res.end();
+          }).catch(error => res.status(400).send(error));
+        }
+        function  userLogin(req, res) {
+           let test=-1;
+            userdb.findOne({
+                where:{
+                    email: "JonDavies30000@hotmail.com", password: "password"
+                }
+              })
+              .then(userdb => {
+                
+                console.log(`Found user: ${userdb.email}`);
+                
+              }).catch(userdbd =>{
+                userdb.findOne({
+                    where:{
+                        email: "JonDavies30000@hotmail.com"
+                    }
+                  }).then(userdb => {
+                    console.log(`Password Was Wrong`);
+                  }).catch(outcome =>{
+                    console.log('Email is wrong')
+            });
+        });
+        res.end();
+    }
+           
+            // return userdb.findOne({
+            //     where:{
+            //         email: "JonDavies3@hotmail.com", password: "computer"
+            //     }
+            //   })
+            //   .then(userdb => {
+            //     console.log(`Found user: ${userdb.email}`);
+            //     res.end();
+            //   }).catch(error => { console.log("failed");res.status(400).send(error)});
+          //  }
+    
+function  usercreate2(req, res) {
+    const newData = {
+        about: 'Maxy-boi-boi',
+        imageUrl: "alt",
+        dob: "2019-09-09",
+        country: "Canada",
+        };
+          
+        userdb.update(newData, {where: { email: 'JonDavies4@hotmail.com' } })
+        .then(userdb => {
+        res.status(201).send(userdb)
+        }).catch(error => res.status(400).send(error));
+    }
 
 const profileCreaterCancel = (req, res, next) => {
-    db.cancelProfilecreation(req,res);
-
+userdb.destroy({where: { email: 'JonDavies2@hotmail.com' } })
+.then(userdb => {
+  res.status(201).send(userdb)
+}).catch(error => res.status(400).send(error));
+}
+module.exports = {
+    usercreate1:usercreate1,
+    usercreate2:usercreate2,
+    profileCreaterCancel:profileCreaterCancel,
+    userfindUser:userfindUser,
+    userLogin:userLogin
 };
-  module.exports = {
-    profileCreator1:profileCreator1,
-    profileCreator2:profileCreator2,
-    profileCreaterCancel:profileCreaterCancel
-  };
+
--- routes/knowledgebase.js
+++ routes/knowledgebase.js
@@ -1,17 +1,21 @@
+
 const express = require('express');
 
 const router = express.Router();
 
 const logincontroller = require('../controllers/loginController');
-const messagecontroller = require('../controllers/messageController');
-const postcontroller = require('../controllers/postController');
+//const messagecontroller = require('../controllers/messageController');
+//const postcontroller = require('../controllers/postController');
 const profilecontroller = require('../controllers/profileController');
 
 router
-    .get('/', logincontroller.login)
-    .get('/profilecreationstep1',profilecontroller.profileCreator1)
-    .get('/profilecreationstep2',profilecontroller.profileCreator2)
+    .get('/profilecreationstep1',profilecontroller.usercreate1)
+    .get('/profilecreationstep2',profilecontroller.usercreate2)
     .get('/profilecreationcancel', profilecontroller.profileCreaterCancel)
-    .get('/profilelogin', logincontroller.checklogin)
+    .get('/profilefind', profilecontroller.userfindUser)
+    .get('/userlogin', profilecontroller.userLogin)
+    //.get('/', logincontroller.login)
+
+    // .get('/profilelogin', logincontroller.checklogin)
 
 module.exports = router;
