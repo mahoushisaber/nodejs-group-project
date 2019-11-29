319848d59662030bddd328a383eff9fc428465be
John <mahoushisaber@gmail.com>
2019-11-29 11:17:51 -0800

    some changes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const expressHbs = require('express-handlebars');
+var Handlebars = require('handlebars');
 
 const app = express();
 const routes = require('./routes/index');
@@ -11,11 +12,72 @@ app.use(bodyParser.urlencoded({
     extended: true
 })); 
 
+Handlebars.registerHelper("inc", function(value, options)
+{
+    return parseInt(value) + 1;
+});
+
+Handlebars.registerHelper('xIf', function (lvalue, operator, rvalue, options) {
+
+  var operators, result;
+
+  if (arguments.length < 3) {
+      throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
+  }
+
+  if (options === undefined) {
+      options = rvalue;
+      rvalue = operator;
+      operator = "===";
+  }
+
+  operators = {
+      '==': function (l, r) { return l == r; },
+      '===': function (l, r) { return l === r; },
+      '!=': function (l, r) { return l != r; },
+      '!==': function (l, r) { return l !== r; },
+      '<': function (l, r) { return l < r; },
+      '>': function (l, r) { return l > r; },
+      '<=': function (l, r) { return l <= r; },
+      '>=': function (l, r) { return l >= r; },
+      'typeof': function (l, r) { return typeof l == r; }
+  };
+
+  if (!operators[operator]) {
+      throw new Error("'xIf' doesn't know the operator " + operator);
+  }
+
+  result = operators[operator](lvalue, rvalue);
+
+  if (result) {
+      return options.fn(this);
+  } else {
+      return options.inverse(this);
+  }
+});
+
 app.engine('hbs',expressHbs ({
     defaultLayout: 'index',
     layoutsDir: 'views/layouts/',
     partialsDir: ['views/partials/'],
     extname: 'hbs'
+  }),
+  
+  'handlebars', expressHbs({
+    helpers: {
+      // Function to do basic mathematical operation in handlebar
+      math: function (lvalue, operator, rvalue) {
+        lvalue = parseFloat(lvalue);
+        rvalue = parseFloat(rvalue);
+        return {
+          "+": lvalue + rvalue,
+          "-": lvalue - rvalue,
+          "*": lvalue * rvalue,
+          "/": lvalue / rvalue,
+          "%": lvalue % rvalue
+        }[operator];
+      }
+    }
   })
 );
 app.set('views', path.join(__dirname, 'views'));
